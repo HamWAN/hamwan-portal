eccae683d2d1de8a53c8634ad49bc6ee4c1430d8
Tom Hayward <esarfl+github@gmail.com>
2014-04-17 15:35:39 -0700

    Host: Add search and extra columns to admin.

--- portal/admin.py
+++ portal/admin.py
@@ -2,7 +2,17 @@ from models import *
 from django.contrib import admin
 
 
-admin.site.register(Host)
+class HostAdmin(admin.ModelAdmin):
+    list_display = (
+        '__unicode__',
+        'owner',
+        'eth_ipv4',
+        'wlan_ipv4',
+    )
+    list_filter = ('owner',)
+    search_fields = ('name', 'eth_ipv4', 'wlan_ipv4')
+    save_as = True
+admin.site.register(Host, HostAdmin)
 
 class SubnetAdmin(admin.ModelAdmin):
     list_display = (
