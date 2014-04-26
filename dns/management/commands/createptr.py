93f14fdb38ffc777c705142c9198db652830132a
Tom Hayward <esarfl+github@gmail.com>
2014-04-25 17:20:51 -0700

    dns: add management command to insert generic PTRs.

---
+++ dns/management/commands/createptr.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand, CommandError
+from dns.models import Record, Domain
+from portal.network import reverse
+from ipaddr import IPAddress, IPNetwork
+
+class Command(BaseCommand):
+    args = '<network> <pattern>'
+    defaultnetwork = IPNetwork('44.24.240.0/20')
+    defaultpattern = '%(0)s-%(1)s-%(2)s-%(3)s.ip.hamwan.net'
+    help = 'Creates a PTR for all IPs without a current PTR.\n\n' \
+           'The default pattern is: %s' % defaultpattern
+
+    def handle(self, *args, **options):
+        if len(args) == 0:
+            network, pattern = self.defaultnetwork, self.defaultpattern
+        if len(args) == 1:
+            network, pattern = IPNetwork(args[0]), self.defaultpattern
+        if len(args) == 2:
+            network, pattern = IPNetwork(args[0]), args[1]
+
+        for ip in network.iterhosts():
+            name = reverse(ip)
+            content = pattern % dict(
+                [(str(k), v) for k, v in enumerate(str(ip).split('.'))])
+
+            # print "%s\tcreated: %s" % (
+            #     Record.objects.get_or_create(domain=self.get_domain(content),
+            #         name=content, type='A', defaults={'content': str(ip)}))
+            print "%s\t\tcreated: %s" % (
+                Record.objects.get_or_create(domain=self.get_domain(name),
+                    name=name, type='PTR', defaults={'content': content}))
+
+    def get_domain(self, name):
+        if not hasattr(self, 'domains'):
+            self.domains = {}
+            for domain in Domain.objects.all():
+                self.domains[domain.name] = domain
+
+        for (k, v) in self.domains.items():
+            if name.endswith(k):
+                return v
