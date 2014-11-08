HamWAN/hamwan-portal

commit 06db7220fce9868b992d54e0baba2ba38d6e5578

Add Site model.

portal/admin.py
@@ -20,15 +20,21 @@ class IPAddressInline(admin.TabularInline):
     extra = 1
 
 
+admin.site.register(Site)
+
+
 class HostAdmin(admin.ModelAdmin):
     list_display = (
+        'site',
         '__unicode__',
         'owner',
         'get_ips'
     )
-    list_filter = ('owner',)
-    search_fields = ('name',)
+    list_display_links = '__unicode__',
+    list_filter = ('owner', 'site')
+    search_fields = ('name', 'site')
     save_as = True
+    save_on_top = True
     inlines = [IPAddressInline]
 
     def get_ips(self, obj):

portal/forms.py
@@ -10,6 +10,7 @@ class HostForm(forms.ModelForm):
     class Meta:
         model = Host
         # fields = '__all__'  # needed in future version of Django
+        exclude = ['site']
 
 
 class UserHostForm(HostForm):

portal/migrations/0013_auto__add_site.py
@@ -1,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Site'
+        db.create_table(u'portal_site', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
+            ('latitude', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('longitude', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('status', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('comment', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'portal', ['Site'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Site'
+        db.delete_table(u'portal_site')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'portal.host': {
+            'Meta': {'object_name': 'Host'},
+            'admins': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'authorized_hosts'", 'blank': 'True', 'to': u"orm['auth.User']"}),
+            'eth_mac': ('portal.fields.MACAddressField', [], {'max_length': '17', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '242'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'hosts_owned'", 'null': 'True', 'to': u"orm['auth.User']"}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '24'}),
+            'wlan_mac': ('portal.fields.MACAddressField', [], {'max_length': '17', 'null': 'True', 'blank': 'True'})
+        },
+        u'portal.ipaddress': {
+            'Meta': {'ordering': "['ip']", 'object_name': 'IPAddress'},
+            'auto_dns': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
+            'host': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'ipaddresses'", 'to': u"orm['portal.Host']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interface': ('django.db.models.fields.CharField', [], {'max_length': '242', 'null': 'True', 'blank': 'True'}),
+            'ip': ('portal.network.IPAddressField', [], {'unique': 'True'}),
+            'primary': ('django.db.models.fields.BooleanField', [], {})
+        },
+        u'portal.site': {
+            'Meta': {'object_name': 'Site'},
+            'comment': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'})
+        },
+        u'portal.subnet': {
+            'Meta': {'ordering': "['network']", 'object_name': 'Subnet'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'network': ('portal.network.IPNetworkField', [], {'unique': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'subnets_owned'", 'null': 'True', 'to': u"orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['portal']

portal/migrations/0014_auto__add_field_host_site.py
@@ -1,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Host.site'
+        db.add_column(u'portal_host', 'site',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['portal.Site'], null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Host.site'
+        db.delete_column(u'portal_host', 'site_id')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'portal.host': {
+            'Meta': {'object_name': 'Host'},
+            'admins': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'authorized_hosts'", 'blank': 'True', 'to': u"orm['auth.User']"}),
+            'eth_mac': ('portal.fields.MACAddressField', [], {'max_length': '17', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '242'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'hosts_owned'", 'null': 'True', 'to': u"orm['auth.User']"}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.Site']", 'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '24'}),
+            'wlan_mac': ('portal.fields.MACAddressField', [], {'max_length': '17', 'null': 'True', 'blank': 'True'})
+        },
+        u'portal.ipaddress': {
+            'Meta': {'ordering': "['ip']", 'object_name': 'IPAddress'},
+            'auto_dns': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
+            'host': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'ipaddresses'", 'to': u"orm['portal.Host']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interface': ('django.db.models.fields.CharField', [], {'max_length': '242', 'null': 'True', 'blank': 'True'}),
+            'ip': ('portal.network.IPAddressField', [], {'unique': 'True'}),
+            'primary': ('django.db.models.fields.BooleanField', [], {})
+        },
+        u'portal.site': {
+            'Meta': {'object_name': 'Site'},
+            'comment': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'})
+        },
+        u'portal.subnet': {
+            'Meta': {'ordering': "['network']", 'object_name': 'Subnet'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'network': ('portal.network.IPNetworkField', [], {'unique': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'subnets_owned'", 'null': 'True', 'to': u"orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['portal']

portal/models.py
@@ -47,6 +47,22 @@ class DomainSortManager(models.Manager):
             return super(DomainSortManager, self).get_query_set()
 
 
+class Site(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    latitude = models.FloatField(null=True, blank=True,
+        help_text="Decimal (e.g., 00.0000)")
+    longitude = models.FloatField(null=True, blank=True,
+        help_text="Decimal (e.g., 000.0000)")
+    status = models.CharField(max_length=30, blank=True)
+    comment = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
 class Host(models.Model):
     """Tracks any asset on the network."""
     objects = DomainSortManager()
@@ -54,6 +70,7 @@ class Host(models.Model):
     name = models.CharField(max_length=242, unique=True,
         validators=[domain_validator])
     type = models.CharField(max_length=24, choices=HOST_TYPES)
+    site = models.ForeignKey(Site, null=True, blank=True)
 
     owner = models.ForeignKey('auth.User', null=True, blank=True,
         related_name="hosts_owned", help_text="Warning: changing this field "

portal/templates/portal/hosttable.html
@@ -2,6 +2,7 @@
     <table class="table table-striped table-condensed">
       <thead>
         <tr>
+          <th>Site</th>
           <th>Owner</th>
           <th class="text-right">Host FQDN</th>
           <th>Interfaces</th>
@@ -11,6 +12,7 @@
       <tbody>
     {% for host in hosts %}
         <tr>
+          <td>{% if host.site %}{{ host.site }}{% endif %}</td>
           <td>{{ host.owner }}</td>
           <td class="text-right"><a href="{% url 'portal.views.host_detail' host.name %}">{{ host.fqdn }}</a></td>
           <td>{{ host.ipaddresses.all|join:"<br>" }}</td>

commit af54de97fdb20990e81bc69a576ea849b487cf4d

Host: fix search in admin (regression from 06db722)

portal/admin.py
@@ -32,7 +32,7 @@ class HostAdmin(admin.ModelAdmin):
     )
     list_display_links = '__unicode__',
     list_filter = ('owner', 'site')
-    search_fields = ('name', 'site')
+    search_fields = ('name', 'site__name')
     save_as = True
     save_on_top = True
     inlines = [IPAddressInline]