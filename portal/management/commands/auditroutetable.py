c75c5de3a4b042cbb2ffc47cd5201dd2bb023cb4
Tom Hayward <esarfl+github@gmail.com>
2017-08-28 19:45:43 -0700

    auditroutetable: hide routes delegated to users, add supernets option, hostname lookup.

--- portal/management/commands/auditroutetable.py
+++ portal/management/commands/auditroutetable.py
@@ -2,10 +2,13 @@ import subprocess
 from optparse import make_option
 from ipaddr import IPNetwork
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.core.management.base import BaseCommand, CommandError
 from portal.models import Subnet, IPAddress
 
 
+# Audit these networks. Modify as needed.
 NETWORKS = map(IPNetwork, [
     '44.24.240.0/20',
     '44.25.0.0/16',
@@ -29,18 +32,26 @@ class bcolors:
 
 
 class Command(BaseCommand):
-    help = 'Compares route list to documented subnets.'
+    help = 'Compares route table to documented subnets.'
     option_list = BaseCommand.option_list + (
-        make_option('--no-color',
+        make_option(
+            '--no-color',
             action='store_true',
             dest='no_color',
             default=False,
             help='Disables colorized output.'),
-        make_option('-q', '--quiet',
+        make_option(
+            '-q', '--quiet',
             action='store_true',
             dest='quiet',
             default=False,
-            help='Hide documented networks.'),
+            help='Hide documented routes.'),
+        make_option(
+            '-s', '--list-supernets',
+            action='store_true',
+            dest='supernets',
+            default=False,
+            help='List supernets of undocumented routes.'),
         )
 
     def handle(self, *args, **options):
@@ -48,9 +59,18 @@ class Command(BaseCommand):
         self.audit_subnets()
 
     def audit_subnets(self):
-        subnets = [subnet.network for subnet in Subnet.objects.all()]
-        addresses = [ipaddress.ip for ipaddress in IPAddress.objects.all()]
-        # self.stdout.write(str(subnets))
+        subnet_map = dict((i.network, i) for i in Subnet.objects.all())
+        usernet_map = dict((i.network, i) for i in Subnet.objects.exclude(
+            owner=User.objects.get(username='HamWAN')))
+        addresses = dict((IPNetwork(i.ip), i) for i in
+                         IPAddress.objects.select_related('host').all())
+
+        def format_subnet(routedst):
+            owner = subnet_map[routedst].owner
+            return str(routedst).ljust(17) + \
+                (owner and "[%s] " % str(owner) or "") + \
+                subnet_map[routedst].notes
+
         output = subprocess.check_output(["ip", "r"])
         for line in output.split('\n'):
             try:
@@ -60,21 +80,46 @@ class Command(BaseCommand):
 
             if any(routedst in net for net in RFC1918):
                 self.write_color(bcolors.FAIL,
-                                 str(routedst) + " in RFC 1918")
+                                 str(routedst).ljust(17) + "in RFC 1918")
                 continue
 
+            # route is one of our supernets, skip it
+            if routedst in NETWORKS:
+                continue
+
+            # route is not in HamWAN space, skip it
             if all(routedst not in net for net in NETWORKS):
                 continue
 
-            if routedst in subnets:
+            usersupernet = filter(lambda x: routedst in x, usernet_map.keys())
+            # matched routes are OK
+            if routedst in subnet_map:
                 if not self.options['quiet']:
-                    self.write_color(bcolors.OKGREEN, str(routedst))
+                    self.write_color(bcolors.OKGREEN, format_subnet(routedst))
+            # subnets of user-assigned networks are OK
+            elif usersupernet:
+                if not self.options['quiet']:
+                    self.stdout.write('%sdelegated to %s' % (
+                                      str(routedst).ljust(17),
+                                      str(subnet_map[usersupernet[0]].owner)))
+            # /32s that match host records are OK
             elif routedst in addresses:
                 if not self.options['quiet']:
-                    self.stdout.write(str(routedst) + " host matched")
+                    self.stdout.write(str(routedst).ljust(17) +
+                                      addresses[routedst].fqdn())
             else:
                 self.write_color(bcolors.FAIL,
-                                 str(routedst) + " not documented")
+                                 str(routedst).ljust(17) + "not documented")
+                if self.options['supernets']:
+                    supernets = sorted(
+                        filter(lambda x: routedst in x, subnet_map.keys()),
+                        key=lambda x: x.prefixlen,
+                        reverse=True)
+                    if supernets:
+                        self.stdout.write('  known supernets:')
+                        for net in supernets:
+                            self.stdout.write("    " + format_subnet(net))
+                        self.stdout.write("\n")
 
     def write_color(self, color, message):
         if self.options['no_color']:
