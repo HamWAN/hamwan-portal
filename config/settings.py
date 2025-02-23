433180b5ead0a73d4fda931551d4197e83a6caea
Doug Kingston <dpk@randomnotes.org>
2025-02-23 17:59:21 -0500

    Numerous small changes from installation on SRV2.

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/www/dns-portal/config/static/'
+STATIC_ROOT = '/var/www/dns-portal/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
