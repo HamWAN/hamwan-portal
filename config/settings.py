c8c03330d84b94b36c1323aaa06cc15fafe3969f
Doug Kingston <dpk@randomnotes.org>
2025-02-23 19:24:44 -0500

    Add production logging stanza.

--- config/settings.py
+++ config/settings.py
@@ -189,7 +189,31 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
+PRODUCTION_LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+DEBUG_LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
@@ -221,6 +245,8 @@ LOGGING = {
         },
     }
 }
+
+LOGGING=DEBUG_LOGGING
 
 AMPR_DNS_FROM = "portal-dns-bot@example.com"
 AMPR_DNS_TO = "ampraddr@example.com"
